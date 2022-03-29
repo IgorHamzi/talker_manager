be5febbbe73a8715515c3c8c208f4dd0ed9d8d85
IgorHamzi <igorwhamzi@gmail.com>
2022-03-29 09:32:12 -0300

    adiciona requisito 5

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+const jsonTalken = './talker.json';
+
 app.get('/talker', async (_req, res) => {
-  const getTalker = await fs.readFile('./talker.json', 'utf8');
+  const getTalker = await fs.readFile(jsonTalken, 'utf8');
 
   if (!getTalker) return res.status(HTTP_OK_STATUS).json(JSON.parse([]));
 
@@ -41,7 +43,7 @@ app.get('/talker', async (_req, res) => {
 
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
-  const getTalker = await fs.readFile('./talker.json', 'utf8');
+  const getTalker = await fs.readFile(jsonTalken, 'utf8');
   const getTalkerId = JSON.parse(getTalker).find((element) => element.id === Number(id));
 
   if (!getTalkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -63,7 +65,7 @@ app.post('/talker',
   watchedAtValidate,
   async (req, res) => {
     const { name, age, talk } = req.body;
-    const talkers = await fs.readFile('./talker.json', 'utf8')
+    const talkers = await fs.readFile(jsonTalken, 'utf8')
       .then((talker) => JSON.parse(talker));
 
     const addTalker = {
@@ -73,7 +75,29 @@ app.post('/talker',
       talk,
     };
 
-    await fs.writeFile('./talker.json', JSON.stringify([...talkers, addTalker]));
+    await fs.writeFile(jsonTalken, JSON.stringify([...talkers, addTalker]));
 
     return res.status(201).json(addTalker);
+});
+
+app.put('/talker/:id',
+  tokenValidate,
+  nameValidate,
+  ageValidate,
+  talkValidate,
+  rateValidate,
+  watchedAtValidate,
+  async (req, res) => {
+    const { id } = req.params;
+    const { name, age, talk } = req.body;
+    const talkers = await fs.readFile(jsonTalken, 'utf8')
+      .then((talker) => JSON.parse(talker));
+    const talkerId = talkers.find((t) => t.id === Number(id));
+    talkerId.name = name;
+    talkerId.age = age;
+    talkerId.talk = talk;
+  
+    await fs.writeFile(jsonTalken, JSON.stringify(talkers));
+
+    return res.status(200).json({ id: Number(id), name, age, talk });
 });
