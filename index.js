45e80544141dc34b0c54b92731bfc2d00c25e3e8
IgorHamzi <igorwhamzi@gmail.com>
2022-03-28 21:19:11 -0300

    adiciona requisito 2

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ app.listen(PORT, () => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const getTalker = await fs.readFile('./talker.json');
+  const getTalker = await fs.readFile('./talker.json', 'utf8');
+
   if (!getTalker) return res.status(HTTP_OK_STATUS).json(JSON.parse([]));
+
   return res.status(HTTP_OK_STATUS).json(JSON.parse(getTalker));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const getTalker = await fs.readFile('./talker.json', 'utf8');
+  const getTalkerId = JSON.parse(getTalker).find((element) => element.id === Number(id));
+
+  if (!getTalkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(HTTP_OK_STATUS).json(getTalkerId);
 });
