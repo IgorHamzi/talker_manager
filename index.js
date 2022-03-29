765c09b9a4cb5e3a78d1647ccb0c00f3f63c346c
IgorHamzi <igorwhamzi@gmail.com>
2022-03-28 21:06:44 -0300

    adiciona requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const fs = require('fs').promises;
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -15,4 +15,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const getTalker = await fs.readFile('./talker.json');
+  if (!getTalker) return res.status(HTTP_OK_STATUS).json(JSON.parse([]));
+  return res.status(HTTP_OK_STATUS).json(JSON.parse(getTalker));
 });
